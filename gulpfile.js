622ec83c01c796e5a5ce9fad4cded75241874d4a
CaioMorenoGoulart <caiogoulartprogramacao@gmail.com>
2021-05-13 14:25:26 -0300

    feat: Banner complited and adjusts in Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,19 @@ var { src, series, dest, watch } = require('gulp');
 
 var sourcemaps = require('gulp-sourcemaps');
 
-var { stream, init, reload } = require('browser-sync').create();
+var { init, reload } = require('browser-sync').create();
 
 var concat = require('gulp-concat');
 var terser = require('gulp-terser');
 var sass = require('gulp-sass');
+var htmlmin = require('gulp-htmlmin');
 
-function compilaSass () {
-  return src('./src/**/**/*.scss')
-    .pipe(sass())
-    .pipe(dest('./dest'))
-    .pipe(stream());
-}
 
 function sassTask () {
   return src('./src/**/**/*.scss')
     .pipe(concat('style.min.css'))  
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(dest('./dest/'))
+    .pipe(dest('./deploy/src/css/'))
 }
 
 function jsTask () {
@@ -29,25 +24,42 @@ function jsTask () {
     .pipe(concat('index.min.js'))
     .pipe(terser())
     .pipe(sourcemaps.write('.'))
-    .pipe(dest('./dest/'))
+    .pipe(dest('./deploy/src/js/'))
+}
+
+function htmlTask () {
+  return src('./index.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('./deploy/'))
+}
+
+function imgTask () {
+  return (
+    src('./src/img/**/*.svg') 
+    .pipe(dest('./deploy/src/img/')),
+    src('./src/img/**/*.png')
+    .pipe(dest('./deploy/src/img/'))
+    )
 }
 
 function listen() {
   init({
     server: {
-      baseDir: './'
+      baseDir: './deploy/'
     }
   });
   watch('./src/**/**/*.scss', sassTask)
   watch('./src/**/**/*.js', jsTask)
-  watch('./dest/*.css').on('change', reload)
-  watch('./dest/*.js').on('change', reload)
+  watch('./index.html', htmlTask)
+  watch('./deploy/src/css/*.css').on('change', reload)
+  watch('./deploy/src/js/*.js').on('change', reload)
   watch('./*.html').on('change', reload)
 }
 
-exports.compilaSass = compilaSass;
 exports.listen = listen;
 exports.jsTask = jsTask;
 exports.sassTask = sassTask;
+exports.htmlTask = htmlTask;
+exports.imgTask= imgTask;
 exports.default = series(jsTask, sassTask, listen);
-exports.build = series(jsTask, sassTask);
+exports.build = series(jsTask, sassTask, htmlTask, imgTask);
