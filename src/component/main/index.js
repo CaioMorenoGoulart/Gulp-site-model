bde61de6fc53c5f874399e5b2d1cdc96e3dabfcd
CaioMorenoGoulart <caiogoulartprogramacao@gmail.com>
2021-05-31 10:12:13 -0300

    feat: Site model finished

---
+++ src/component/main/index.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  $("#nossaMissao, #quemSomos").on('click', function(e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: ($(target).offset().top)
+    }, 1000);
+  });
+  $("#servicostop").on('click', function(e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: ($(target).offset().top)
+    }, 1000);
+  });
+  $("#contato").on('click', function(e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: ($(target).offset().top)
+    }, 1000);
+  });
+  $("#topPage").on('click', function() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  });
+});
+
+var scrolll
+var tamanho
+
+if($(window).width() > 991) {
+  $(window).scroll(function() {
+      scrolll = $('html').scrollTop();
+      tamanho = $(window).height();
+      if (scrolll >= tamanho)
+        $(".side-bar").addClass("scrolled")
+      else
+        $(".side-bar").removeClass("scrolled")
+  });
+}
